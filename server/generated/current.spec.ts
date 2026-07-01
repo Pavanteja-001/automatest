c17aa5c9d0ae384f025ed33b6a714045f6be5bd2
naveen seeram <seeramnaveenkumar673@gmail.com>
2026-07-01 11:46:46 +0530

    commit 02

--- server/generated/current.spec.ts
+++ server/generated/current.spec.ts
@@ -1,4 +1,17 @@
 import { test, expect } from '@playwright/test';
 
 test('test', async ({ page }) => {
+  await page.goto('https://www.reaidy.io/');
+  await page.locator('.relative.w-20').click();
+  await page.locator('.relative.w-20').click();
+  await page.getByRole('button', { name: 'MERN' }).click();
+  await page.getByRole('button', { name: 'Java' }).click();
+  await page.getByRole('button', { name: 'AI/ML' }).click();
+  await page.getByRole('button', { name: 'Digital marketing' }).click();
+  await page.getByRole('button', { name: 'Business Development' }).click();
 });
+
+
+
+
+
